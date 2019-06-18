2ae880f66d9594a6aefd9eaae86f8efcc7b4786e
Varun Rathi <varun.rathi@jeevansathi.com>
2019-06-19 03:20:26 +0530

    Search sorted Array solution

--- Search_sorted_matrix.cpp
+++ Search_sorted_matrix.cpp
@@ -1,3 +1,26 @@
+//https://www.algoexpert.io/questions/Search%20In%20Sorted%20Matrix
+//https://www.geeksforgeeks.org/search-in-row-wise-and-column-wise-sorted-matrix/
+
+/* 
+Given an n x n matrix and a number x, find the position of x in the matrix if it is present in it. Otherwise, print “Not Found”. In the given matrix, every row and column is sorted in increasing order. The designed algorithm should have linear time complexity.
+Example :
+
+Input : mat[4][4] = { {10, 20, 30, 40},
+                      {15, 25, 35, 45},
+                      {27, 29, 37, 48},
+                      {32, 33, 39, 50}};
+              x = 29
+Output : Found at (2, 1)
+
+Input : mat[4][4] = { {10, 20, 30, 40},
+                      {15, 25, 35, 45},
+                      {27, 29, 37, 48},
+                      {32, 33, 39, 50}};
+              x = 100
+Output : Element not found
+
+*/
+
 #include <vector>
 using namespace std;
 
